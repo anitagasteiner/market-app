d345a285630474e4fc7a5c071f05b4512d4b98ad
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-10-20 17:22:32 +0200

    MarketsView Mixin

--- market_app/api/views.py
+++ market_app/api/views.py
@@ -4,21 +4,32 @@ from rest_framework import status
 from.serializers import MarketSerializer, SellerSerializer, MarketHyperlinkedSerializer, ProductSerializer
 from market_app.models import Market, Seller, Product
 from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 
 
-class MarketsView(APIView):
+class MarketsView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
 
-    def get(self, request):
-        markets = Market.objects.all()
-        serializer = MarketHyperlinkedSerializer(markets, many=True, context={'request': request}, fields=('id', 'name', 'net_worth', 'url'))
-        return Response(serializer.data)
+    queryset = Market.objects.all()
+    serializer_class = MarketSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
     
-    def post(self, request):
-        serializer = MarketSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    # def get(self, request):
+    #     markets = Market.objects.all()
+    #     serializer = MarketHyperlinkedSerializer(markets, many=True, context={'request': request}, fields=('id', 'name', 'net_worth', 'url'))
+    #     return Response(serializer.data)
+    
+    # def post(self, request):
+    #     serializer = MarketSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # @api_view(['GET', 'POST']) # decorator
